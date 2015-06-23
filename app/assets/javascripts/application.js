51132f059f356c1cb7489227bc4bacc6d6e313c2
Daisy Magnus-Aryitey <daisymagnusaryitey@gmail.com>
2015-06-23 09:16:38 -0400

    Disable button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+// When the button to submit the form is clicked, disable the button so that the
+// user cannot double-click. Try not to use jQuery, but if you have to, so be it.
+
+function disableButton() {
+    document.getElementById("button").disabled = true;
+}
+
+onclick="disableButton()"
